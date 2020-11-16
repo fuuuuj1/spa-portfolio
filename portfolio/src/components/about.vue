5d5dabb61ab0d2ad935e466665e92c1bc6cef171
fuuuu <sanchan.fuu@gmail.com>
2020-11-17 00:16:19 +0900

    profileの作成

--- portfolio/src/components/about.vue
+++ portfolio/src/components/about.vue
@@ -2,17 +2,33 @@
   <div class="about-me">
 
     <div id="profile">
-      <h1>Profile</h1>
-      <div class="profile-photo"></div>
+      <v-container>
+      <h1 class="heading">Profile</h1>
+
+      <v-row>
+
+      <v-col cols="6">
       <div class="profile-title">
-        <h2>藤木 章吾</h2>
-        <h3>エンジニアを目指す診療放射線技師</h3>
+        <h3>藤木 章吾</h3>
+        <p>エンジニアを目指す診療放射線技師</p>
       </div>
       <div class="profile-text">
         <p>1993年3月、広島県生まれ。大学を卒業後、京都府宇治市における救急の中核病院に診療放射線技師として就職。
-          2020年1月より地元の病院に転職。患者さんや他の医療従事者のために働いていくことも好きであるが、より多くの人のためにサービスを提供できるWEBエンジニアに可能性を感じ、2020年4月より独学でプログラミング学習開始。
+          2020年1月より地元の病院に転職。患者さんや他の医療従事者のために働いていくことも好きであるが、より多くの人のためにサービスを提供できるWEBエンジニアになりたいと思い、2020年4月より独学でプログラミング学習開始。
         </p>
       </div>
+      </v-col>
+
+      <v-col cols=6>
+      <div class="profile-photo">
+        <v-img
+          v-bind:src="profile_src">
+        </v-img>
+      </div>
+      </v-col>
+
+      </v-row>
+      </v-container>
     </div>
 
     <div id="favorite">
@@ -20,45 +36,67 @@
     </div>
 
     <div id="history">
+
+      <v-container :fulid="true">
       <h1>Work history</h1>
 
-      <div class="first-work">
+        <v-row>
 
-        <div class="first-work-title">
-          <h3>2014 - 2019</h3>
-          <h4>宇治徳洲会病院</h4>
+            <v-col cols="8">
+              <div class="first-work-title">
+                <h3>2014 - 2019 宇治徳洲会病院</h3>
+              </div>
+
+              <div class="first-work-text">
+                <p>簡単な説明文</p>
+                  <ul>
+                    <li>冷静に行動する</li>
+                    <li>順序立てて仕事をこなしていく</li>
+                    <li>どのような方とも円滑にコミュニケーションをとる</li>
+                  </ul>
+                <p>慌ただしい救急の現場であるが故に医療の知識だけでなく、この3つの力が鍛えられました</p>
+              </div>
+            </v-col>
+
+          <v-col cols="4">
+            <div class="first-work-image">
+              <v-img
+                lazy-src="https://picsum.photos/id/11/10/6"
+                max-height="300"
+                max-width="368"
+                src="https://picsum.photos/id/11/500/300">
+              </v-img>
+            </div>
+          </v-col>
+
+        </v-row>
+
+      <v-row>
+
+        <v-col cols="4">
+        <div class="second-work-image">
+          <v-img
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="300"
+            max-width="368"
+            src="https://picsum.photos/id/11/500/300">
+          </v-img>
         </div>
+        </v-col>
 
-        <div class="first-work-text">
-          <p>簡単な説明文</p>
-            <ul>
-              <li>冷静に行動する</li>
-              <li>順序立てて仕事をこなしていく</li>
-              <li>どのような方とも円滑にコミュニケーションをとる</li>
-            </ul>
-          <p>慌ただしい救急の現場であるが故に医療の知識だけでなく、この3つの力が鍛えられました</p>
-        </div>
+        <v-col cols="8">
+          <div class="second-work-title">
+            <h3>2020 - 現在 興生総合病院</h3>
+          </div>
+          <div class="second-work-text">
+            <p>転職の経緯</p>
+            <p>スキルマップの話</p>
+          </div>
+        </v-col>
 
-        <div class="first-work-image"></div>
+      </v-row>
 
-      </div>
-
-      <div class="second-work">
-
-        <div class="second-work-image"></div>
-
-        <div class="second-work-title">
-          <h3>2020 - 現在</h3>
-          <h4>興生総合病院</h4>
-        </div>
-
-        <div class="second-work-text">
-          <p>転職の経緯</p>
-          <p>スキルマップの話</p>
-        </div>
-
-      </div>
-
+      </v-container>
     </div>
 
     <div id="belief">
@@ -74,7 +112,69 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      profile_src: require("@/assets/imags/IMG_0711.jpeg"),
+    }
+  },
+}
 </script>
 
-<style>
+<style scope>
+#profile {
+  height: 100vh;
+  width: 100%;
+  margin-top: 3vh;
+}
+#profile h1 {
+  text-align: left;
+  font-size: 3.5rem;
+  color: #072142;
+  letter-spacing: 0.2em;
+  font-family: "futura_m";
+  margin-right: 3.2rem;
+  padding-top: 1.2rem;
+}
+.profile-title {
+    color: #072142;
+  margin-bottom: 2.4rem;
+}
+.profile-title  h3 {
+  font-size: 2.4rem;
+  letter-spacing: 0.3em;
+  font-weight: bold;
+  margin-bottom: 0.4rem;
+  text-align: left;
+}
+.profile-title p {
+  font-size: 1.4rem;
+    letter-spacing: 0.2em;
+    line-height: 1.3;
+    text-align: left;
+}
+.profile-text {
+  text-align: left;
+  line-height: 2;
+}
+.profile-photo {
+  box-shadow: 0px 63px 124px -20px rgba(7, 33, 66, 0.5);
+}
+#history {
+  height: 90vh;
+  width: 100%;
+}
+#history h1 {
+  text-align: left;
+  text-align: left;
+  font-size: 3.5rem;
+  color: #072142;
+  letter-spacing: 0.12em;
+  font-family: "futura_m";
+  margin-right: 3.2rem;
+  padding-top: 1.2rem;
+}
+.row {
+  margin-top:5vh;
+}
 </style>
